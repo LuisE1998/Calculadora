7dc53a0cb8936490a1460c9c1718453d37bfdee7
luis <luis@DESKTOP-1QJSHH1>
2019-02-11 09:51:06 -0500

    Calculadora agregando main

--- src/calculadora_luis/Calculadora_Luis.java
+++ src/calculadora_luis/Calculadora_Luis.java
@@ -2,28 +2,36 @@ package calculadora_luis;
 
 public class Calculadora_Luis {
 
-    private int ans;
+    private static int ans;
     
     public Calculadora_Luis(){
         ans=0;
     }
     
-    public int suma(int a, int b){
+    public static void main(String[] args){
+        int a=0,b=0;
+        a=suma(2,5);
+        System.out.println("La suma es: "+a);
+        b=adicionar(3);
+        System.out.println("La adicion al es da: "+b);
+    }
+    
+    public static int suma(int a, int b){
         ans=a+b;
         return ans;
     }
     
-    public int resta(int a, int b){
+    public static int resta(int a, int b){
         ans=a-b;
         return ans;
     }
     
-    public int multiplicar(int a, int b){
+    public static int multiplicar(int a, int b){
         ans=a*b;
         return ans;
     }
     
-    public int dividir(int a, int b){
+    public static int dividir(int a, int b){
         if (b==0){
             throw new ArithmeticException("no se puede dividir por cero");
         }
@@ -31,19 +39,19 @@ public class Calculadora_Luis {
         return ans;
     }
     
-    public int potencia(int a, int b){
+    public static int potencia(int a, int b){
         ans=1;
         for(int i=0;i<b;i++)
             ans=ans*a;
         return ans;
     }
     
-    public int adicionar(int n){
+    public static int adicionar(int n){
         ans=ans+n;
         return ans;
     }
     
-    public int sustraer(int n){
+    public static int sustraer(int n){
         ans=ans-n;
         return ans;
     }
