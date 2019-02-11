8fad9e597658a520770af05bc5e78f59a5716f82
luis <luis@DESKTOP-1QJSHH1>
2019-02-11 09:15:51 -0500

    Calculadora version 1

---
+++ src/calculadora_luis/Calculadora_Luis.java
@@ -0,0 +1,50 @@
+package calculadora_luis;
+
+public class Calculadora_Luis {
+
+    private int ans;
+    
+    public Calculadora_Luis(){
+        ans=0;
+    }
+    
+    public int suma(int a, int b){
+        ans=a+b;
+        return ans;
+    }
+    
+    public int resta(int a, int b){
+        ans=a-b;
+        return ans;
+    }
+    
+    public int multiplicar(int a, int b){
+        ans=a*b;
+        return ans;
+    }
+    
+    public int dividir(int a, int b){
+        if (b==0){
+            throw new ArithmeticException("no se puede dividir por cero");
+        }
+        ans=a/b;
+        return ans;
+    }
+    
+    public int potencia(int a, int b){
+        ans=1;
+        for(int i=0;i<b;i++)
+            ans=ans*a;
+        return ans;
+    }
+    
+    public int adicionar(int v){
+        ans=ans+v;
+        return ans;
+    }
+    
+    public int sustraer(int v){
+        ans=ans-v;
+        return ans;
+    }
+}
